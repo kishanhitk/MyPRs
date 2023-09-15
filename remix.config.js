ef2e707cbdb91c46160accc6787a9e640f9d81b5
kishanhitk <kishansharma1231@gmail.com>
2023-09-15 22:59:32 +0530

    init: remix-app

---
+++ remix.config.js
@@ -0,0 +1,24 @@
+/** @type {import('@remix-run/dev').AppConfig} */
+export default {
+  devServerBroadcastDelay: 1000,
+  ignoredRouteFiles: ["**/.*"],
+  server: "./server.ts",
+  serverBuildPath: "functions/[[path]].js",
+  serverConditions: ["workerd", "worker", "browser"],
+  serverDependenciesToBundle: "all",
+  serverMainFields: ["browser", "module", "main"],
+  serverMinify: true,
+  serverModuleFormat: "esm",
+  serverPlatform: "neutral",
+  // appDirectory: "app",
+  // assetsBuildDirectory: "public/build",
+  // publicPath: "/build/",
+  future: {
+    v2_dev: true,
+    v2_errorBoundary: true,
+    v2_headers: true,
+    v2_meta: true,
+    v2_normalizeFormMethod: true,
+    v2_routeConvention: true,
+  },
+};
