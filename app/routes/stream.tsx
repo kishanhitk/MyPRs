af9c00b6dec1248a8c4ed20049ca938c622e73ef
kishanhitk <kishansharma1231@gmail.com>
2023-09-20 23:04:57 +0530

    feat: add defer route

---
+++ app/routes/stream.tsx
@@ -0,0 +1,30 @@
+import { Suspense } from "react";
+import { Await, useLoaderData } from "@remix-run/react";
+import { defer } from "@vercel/remix";
+
+export async function loader({ request }) {
+  const version = process.versions.node;
+
+  return defer({
+    version: sleep(version, 1000),
+  });
+}
+
+function sleep(val, ms) {
+  return new Promise((resolve) => setTimeout(() => resolve(val), ms));
+}
+
+export default function App() {
+  const { version } = useLoaderData();
+
+  return (
+    <div>
+      Hello from React! Node.js version:{" "}
+      <Suspense fallback={"Loading…"}>
+        <Await resolve={version}>
+          {(version) => <strong>{version}</strong>}
+        </Await>
+      </Suspense>
+    </div>
+  );
+}
