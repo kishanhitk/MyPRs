8d4ab5a9cb8e5b4867811cbb8409c847611cfddd
kishanhitk <kishansharma1231@gmail.com>
2023-11-03 20:24:22 +0530

    feat: remove unnecessary route

--- app/routes/edge.tsx
+++
@@ -1,8 +0,0 @@
-import type { LoaderFunctionArgs } from "@vercel/remix";
-
-export const config = { runtime: "edge" };
-
-export const loader = async ({ params, request }: LoaderFunctionArgs) => {
-  const city = request.headers.get("x-vercel-ip-city");
-  return { hello: "world", city: city };
-};
